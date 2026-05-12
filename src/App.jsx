4bc20e6eb3ed1c68da747b1e6bc9085f2baa4556
Kiiiiiirl <kirlos246@gmail.com>
2026-05-12 12:24:48 -0600

    changes 120526

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,135 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect} from 'react'
+import { db,auth } from "./firebase";
+
 import './App.css'
 
+import {createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut} from "firebase/auth";
+import {collection,addDoc,getDocs,doc, setDoc,updateDoc } from "firebase/firestore";
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  // ================LOGIN AND LOGOUT===============================
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function signup() {
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      alert("Account created");
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  async function login() {
+    try {
+      await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      alert("Logged in");
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  async function logout() {
+    await signOut(auth);
+  }
+
+
+
+  // ================GET AND ADD DOCS===============================
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  async function loadData() {
+    const snapshot = await getDocs(collection(db, "users"));
+
+    snapshot.forEach((doc) => {
+      console.log(doc.id, doc.data());
+    });
+  }
+
+  async function saveData() {
+    try {
+      /*
+      await setDoc(doc(db, "users", auth.currentUser.uid),{
+        favoriteColor: "pink",
+        score: 100
+      });
+      */
+
+      
+      await updateDoc(
+        doc(db, "users", auth.currentUser.uid),
+        {
+          favoriteFood: "Pizza"
+        }
+      );
+      
+
+      console.log("Saved!");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
 
   return (
     <>
+      <div>
 
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={signup}>
+          Sign Up
+        </button>
+
+        <button onClick={login}>
+          Login
+        </button>
+
+        <button onClick={logout}>
+          Logout
+        </button>
+
+      </div>
+
+      <div>
+        <input type="text" size={4} placeholder='key'/>
+        <input type="text" size={15} placeholder='value'/>
+      </div>
+
+      <br />
+
+      <div>
+        <button onClick={saveData}>
+          Save Dataaa
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDavfOiy1h41PuBpiGPwdVmAh7km7_BJio",
@@ -18,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
